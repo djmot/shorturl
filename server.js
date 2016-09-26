7b618dbd2b9246bd1d19146295e3f0d2de7aedb2
David <djmzorn@gmail.com>
2016-09-26 22:45:35 +0000

    initial

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+
+app.get('/new/', function(request, response) {
+    response.writeHead(200, { 'Content-Type' : 'text/plain' });
+    response.end("new");
+});
+
+app.get('*', function(request, response) {
+    response.sendFile(path.join(__dirname, '/about.html'));
+});
+
+app.listen(process.env.PORT, process.env.IP);
+console.log("Listening on port " + process.env.PORT);
